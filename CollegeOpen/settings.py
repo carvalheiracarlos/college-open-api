1c3c193aa001d07b75bb3ee6ae39ed3c09632618
carvalheirafc <carvalheirafc@gmail.com>
2022-06-13 20:59:20 -0300

    more settings

--- CollegeOpen/settings.py
+++ CollegeOpen/settings.py
@@ -15,8 +15,8 @@ from dj_database_url import parse as dburl
 from decouple import Csv, config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
