bcb71b3e2a00cec96679004b8cdc2e0d11021507
carvalheirafc <carvalheirafc@gmail.com>
2022-06-13 16:46:25 -0300

    added more settings

--- CollegeOpen/settings.py
+++ CollegeOpen/settings.py
@@ -115,11 +115,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'storage/media/
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
