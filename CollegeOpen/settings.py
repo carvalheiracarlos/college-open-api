d3ec8cdd42813fdbe3caa3d0012f59869ccae851
carvalheirafc <carvalheirafc@gmail.com>
2022-06-13 21:07:40 -0300

    more settings

--- CollegeOpen/settings.py
+++ CollegeOpen/settings.py
@@ -141,10 +141,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'storage/media/'
 
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '../CollegeOpen/static'),)
 
 
 # Default primary key field type
