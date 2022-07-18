f312ee6b9d6e6da54711df9b3814d7dd19be3c58
carvalheiracarlos <carvalheirafc@gmail.com>
2022-07-18 17:08:42 -0300

    added major updates in Disciplines

--- CollegeOpen/Disciplines/models.py
+++ CollegeOpen/Disciplines/models.py
@@ -6,10 +6,25 @@ from CollegeOpen.Locations.models import Location
 
 
 class Discipline(BaseModel):
+    component_types = (
+        ('0', 'DISCIPLINAS'),
+        ('1', 'MODULOS'),
+        ('2', 'BLOCOS'),
+        ('3', 'ATIVIDADE ACADEMICA'),
+    )
+    educational_modes_types = (
+        ('0', 'PRESENCIAL'),
+        ('1', 'REMOTO'),
+    )
+
+    compoent_type = models.SmallIntegerField('Tipo do Componente Curricular', choices=component_types, default=0)
+    educational_mode = models.SmallIntegerField('Modalidades de Educação', choices=educational_modes_types, default=0)
     name = models.CharField('Nome da Disciplina', null=True, max_length=128)
     students = models.ManyToManyField(Student, verbose_name='Turma de Estudantes', blank=True)
     professor = models.ForeignKey(Professor, verbose_name='Professor', null=True, blank=True, on_delete=models.PROTECT)
     location = models.ForeignKey(Location, verbose_name='Localização', null=True, blank=True, on_delete=models.PROTECT)
+    exams = models.PositiveSmallIntegerField('Quantidade de Avaliações', default=3)
+
     
 
     class Meta:
@@ -29,3 +44,10 @@ class CoRequirements(BaseModel):
     class Meta:
         verbose_name = 'Co-requisitos'
         verbose_name_plural = 'Co-requisitos'
+
+class Equivalences(BaseModel):
+    disciplines = models.ManyToManyField(Discipline, verbose_name='equivalencias', blank=True)
+
+    class Meta:
+        verbose_name = 'Equivalencias'
+        verbose_name_plural = 'Equivalencias'
