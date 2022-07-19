f07e9758f9a2baae411e94034fd433e6666233b7
carvalheiracarlos <carvalheirafc@gmail.com>
2022-07-18 21:18:15 -0300

    last modifications in disciplines models

--- CollegeOpen/Disciplines/admin.py
+++ CollegeOpen/Disciplines/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from CollegeOpen.Disciplines.models import (
+    Discipline,
+    PreRequirements,
+    CoRequirements,
+    Equivalences
+)
+
+
+admin.site.register([
+    Discipline,
+    PreRequirements,
+    CoRequirements,
+    Equivalences
+])
--- CollegeOpen/Disciplines/models.py
+++ CollegeOpen/Disciplines/models.py
@@ -20,16 +20,20 @@ class Discipline(BaseModel):
     compoent_type = models.SmallIntegerField('Tipo do Componente Curricular', choices=component_types, default=0)
     educational_mode = models.SmallIntegerField('Modalidades de Educação', choices=educational_modes_types, default=0)
     name = models.CharField('Nome da Disciplina', null=True, max_length=128)
+    code = models.CharField('Código da Disciplina', null=True, max_length=48)
     students = models.ManyToManyField(Student, verbose_name='Turma de Estudantes', blank=True)
     professor = models.ForeignKey(Professor, verbose_name='Professor', null=True, blank=True, on_delete=models.PROTECT)
     location = models.ForeignKey(Location, verbose_name='Localização', null=True, blank=True, on_delete=models.PROTECT)
-    exams = models.PositiveSmallIntegerField('Quantidade de Avaliações', default=3)
-
-    
+    total_exams = models.PositiveSmallIntegerField('Quantidade de Avaliações', default=3)
+    flexible_program = models.BooleanField('Hoário Flexivel ?', default=True)
+    online_registry = models.BooleanField('Matricula Online ?', default=True)
+    description = models.TextField('Descrição da Ementa', null=True, blank=True, max_length=512) 
+    long_description = models.FileField('Descrição da Ementa', null=True, blank=True, upload_to='disciplines/description/') 
 
     class Meta:
         verbose_name = 'Disciplina' 
         verbose_name_plural = 'Disciplinas' 
+
 
 class PreRequirements(BaseModel):
     disciplines = models.ManyToManyField(Discipline, verbose_name='pre-requesitos', blank=True)
@@ -38,12 +42,14 @@ class PreRequirements(BaseModel):
         verbose_name = 'Pre-requisitos'
         verbose_name_plural = 'Pre-requisitos'
 
+
 class CoRequirements(BaseModel):
     disciplines = models.ManyToManyField(Discipline, verbose_name='co-requesitos', blank=True)
 
     class Meta:
         verbose_name = 'Co-requisitos'
         verbose_name_plural = 'Co-requisitos'
+
 
 class Equivalences(BaseModel):
     disciplines = models.ManyToManyField(Discipline, verbose_name='equivalencias', blank=True)
